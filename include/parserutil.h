42f004c82d85ab34fe63064a3deaab470a784f95
Jinbobo <paullee05149745@gmail.com>
2015-03-14 17:04:37 +0800

    Docs

--- include/parserutil.h
+++ include/parserutil.h
@@ -2,5 +2,17 @@
 
 #include "parser.h"
 
+/**
+ * Does nothing
+ *
+ * @param root
+ * @return
+ */
 parseTreeNode *parseTreePrune(parseTreeNode *root);
+/**
+ * Prunes branches that terminate in EPSILON.
+ *
+ * @param root the pointer to the root of the parse tree.
+ * @return new location of the new root.
+ */
 parseTreeNode *parseTreeDeallocateEpsilon(parseTreeNode *root);
